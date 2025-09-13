bbd1c079b399188915f030edb48d3eb9658f037e
Tyuiu.NzvyaginaNY@outlook.com <nadiazvagina455@gmail.com>
2025-09-13 20:03:35 +0500

    Task1

---
+++ Tyuiu.ZvyaginaNY.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZvyaginaNY.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет! ");
+            Console.ReadLine();
+        }
+    }
+}
